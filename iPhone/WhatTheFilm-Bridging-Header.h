2fd1f3c77cd71e774dae5d893204a4fd48c91c85
jfrancop <juliofrp@gmail.com>
2016-05-16 12:59:16 -0700

    add SDWebImage library for image caching

---
+++ iPhone/WhatTheFilm-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  WhatTheFilm-Bridging-Header.h
+//  WhatTheFilm
+//
+//  Created by Julio Franco on 5/16/16.
+//  Copyright © 2016 Julio Franco. All rights reserved.
+//
+
+#ifndef WhatTheFilm_Bridging_Header_h
+#define WhatTheFilm_Bridging_Header_h
+
+#import "SDWebImage/UIImageView+WebCache.h"
+
+
+#endif /* WhatTheFilm_Bridging_Header_h */
