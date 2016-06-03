5b2e56912c23e5d72facd90574c9897df87d3791
Joshua Wallace <jtrwallace@mac.com>
2016-06-02 17:59:12 -0700

    Got sliders working on home page.

--- Web/Node/WhatTheFilm/public/javascripts/index.js
+++ Web/Node/WhatTheFilm/public/javascripts/index.js
@@ -7,6 +7,37 @@ $(document).ready( function() {
         speed: 1200,
         delay: 7500
     });
-    
-    $('#ca-container').contentcarousel();
+
+    $(document).find('.row').each(function () {
+
+        if ($(this).find('.slidee').children().length < 5) {
+            $(this).find('.scrollbar').hide();
+        }
+
+        var $frame  = $(this).find('.frame');
+        var $slidee = $frame.children('ul').eq(0);
+        var $wrap   = $frame.parent();
+
+        // Call Sly on frame
+        $frame.sly({
+            horizontal: 1,
+            itemNav: 'basic',
+            smart: 1,
+            activateOn: 'click',
+            mouseDragging: 1,
+            touchDragging: 1,
+            releaseSwing: 1,
+            startAt: 0,
+            scrollBar: $wrap.find('.scrollbar'),
+            scrollBy: 1,
+            pagesBar: $wrap.find('.pages'),
+            activatePageOn: 'click',
+            speed: 300,
+            elasticBounds: 1,
+            easing: 'easeOutExpo',
+            dragHandle: 1,
+            dynamicHandle: 1,
+            clickBar: 1
+        });
+    });
 });
